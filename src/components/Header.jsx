d01bdc123e505fe7306e5c8aad65440d49324067
bruno <bruno932151@gmail.com>
2021-02-08 22:21:22 -0500

    alistando el entorno de desarrollo

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../assets/styles/Header.scss'
+
+
+const Header = () =>{
+    return(
+        <section>
+
+            <h2>Hola Maestro</h2>
+            <div className="bitcoin" ></div>
+
+        </section>
+        
+    )
+}
+
+export default Header
